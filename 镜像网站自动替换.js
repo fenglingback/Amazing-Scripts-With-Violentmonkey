2d0fe6a6e1b159bc812ea7c4f624e3a621f1b4d8
cxfl <fenglinggod@qq.com>
2024-10-18 09:49:48 +0800

    update

--- 镜像网站自动替换.js
+++ 镜像网站自动替换.js
@@ -1,15 +1,33 @@
 // ==UserScript==
-// @name        镜像网站自动替换
+// @name        Better alternatives to websites
+// @description:zh-CN   网站的更好替代
 // @match       *://raw.githubusercontent.com/*
+// @match       *://www.quora.com/*
+// @match       *://www.reddit.com/*
+// @exclude-match *://www.quora.com/
 // @run-at      document-start
 // @grant       none
 // ==/UserScript==
 
-var newDomain   = "raw.gitmirror.com";
-var newURL      = location.protocol + "//"
-                + newDomain
-                + location.pathname
-                + location.search
-                + location.hash;
 
+var newURL = "";
+const best_gh_raw = "https://g.3344550.xyz/";
+const best_quora = "https://quetre.iket.me";
+const best_reddit = "https://l.opnxng.com";
+
+switch (location.host) {
+    case "raw.githubusercontent.com":
+        newURL = best_gh_raw + location.protocol + "//" + location.host + location.pathname + location.search + location.hash;
+        break;
+    case "www.quora.com":
+        newURL = best_quora + location.pathname + location.search + location.hash;
+        break;
+    case "www.reddit.com":
+        newURL = best_reddit + location.pathname + location.search + location.hash;
+        break;
+    default:
+        break;
+}
+
+console.log("Redirecting to " + newURL);
 location.replace(newURL);
